1a0348b3c8c81d2a3329a822a089bb3bacad4c5c
cinthia <gcinthia919@gmail.com>
2021-08-01 16:38:58 -0300

    terminado

---
+++ app.js
@@ -0,0 +1,10 @@
+const express =require ("express");
+const app = expres();
+const path = require ("path");
+const port = 3030 ;
+
+app.get("/"(req , res )=> res.send "bienvenidos")
+
+app.express ("https://github.com/cinty20/mercado-liebre. git")
+
+app.listen(port,() => console.log("servidor corriendo en el puerto http://localhost:" + port));
