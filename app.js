ed3fe566c8a88063ed9e024987df29640dfaa441
cinthia <gcinthia919@gmail.com>
2021-08-04 16:34:53 -0300

    terminado

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const express =require ("express");
-const app = expres();
+const express = require ("express");
+const app = express();
 const path = require ("path");
 const port = 3030 ;
 
-app.get("/"(req , res )=> res.send "bienvenidos")
-
-app.express ("https://github.com/cinty20/mercado-liebre. git")
+app.get ("/", (req,res) => res.sendFile(path.join(__dirname, "views","home.html")))
+/* configuracion de recursos estaticos(imagenes y estilos)para que se vean las imagenes */
+app.use(express.static("public"));
 
 app.listen(port,() => console.log("servidor corriendo en el puerto http://localhost:" + port));
